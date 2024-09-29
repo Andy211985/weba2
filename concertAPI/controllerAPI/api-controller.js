5c8695422707e46bfa7f908039a37356cd9a127f
Andy211985 <145839766+Andy211985@users.noreply.github.com>
2024-09-29 10:03:17 +0800

    Create GET method to retrieve all categories from the database.

--- concertAPI/controllerAPI/api-controller.js
+++ concertAPI/controllerAPI/api-controller.js
@@ -18,6 +18,17 @@ router.get("/fundraiser", (req, res)=>{
 	})
 })
 
+router.get("/category", (req, res)=>{
+	connection.query("select * from CATEGORY", (err, records,fields)=> {
+		 if (err){
+			 console.error("Error while retrieve the data");
+		 }else{
+			 res.send(records);
+		 }
+	})
+})
+
+
 // router.get("/:id", (req, res)=>{
 // 	connection.query("select * from concert where ConcertID=" + req.params.id, (err, records,fields)=> {
 // 		 if (err){
