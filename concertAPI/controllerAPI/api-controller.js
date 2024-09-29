49a3e05f2d31b00ebb6898727adb61a44b7da833
Andy211985 <145839766+Andy211985@users.noreply.github.com>
2024-09-29 10:17:58 +0800

    Create GET method to retrieve the details of a fundraiser (by ID) from the database.

--- concertAPI/controllerAPI/api-controller.js
+++ concertAPI/controllerAPI/api-controller.js
@@ -8,73 +8,73 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get("/fundraiser", (req, res)=>{
-	connection.query("select * from FUNDRAISER", (err, records,fields)=> {
-		 if (err){
-			 console.error("Error while retrieve the data");
-		 }else{
-			 res.send(records);
-		 }
+router.get("/fundraiser", (req, res) => {
+	connection.query("select * from FUNDRAISER", (err, records, fields) => {
+		if (err) {
+			console.error("Error while retrieve the data");
+		} else {
+			res.send(records);
+		}
 	})
 })
 
-router.get("/category", (req, res)=>{
-	connection.query("select * from CATEGORY", (err, records,fields)=> {
-		 if (err){
-			 console.error("Error while retrieve the data");
-		 }else{
-			 res.send(records);
-		 }
+router.get("/category", (req, res) => {
+	connection.query("select * from CATEGORY", (err, records, fields) => {
+		if (err) {
+			console.error("Error while retrieve the data");
+		} else {
+			res.send(records);
+		}
 	})
 })
 
 router.get("/search", (req, res) => {
-    const { organizer, city, category } = req.query;
-    let query = `
+	const { organizer, city, category } = req.query;
+	let query = `
         SELECT f.*, c.NAME AS CATEGORY_NAME
         FROM FUNDRAISER f
         JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
         WHERE f.ACTIVE = true
     `;
-    const params = [];
+	const params = [];
 
-    if (organizer) {
-        query += ' AND f.ORGANIZER LIKE ?';
-        params.push(`%${organizer}%`);
-    }
+	if (organizer) {
+		query += ' AND f.ORGANIZER LIKE ?';
+		params.push(`%${organizer}%`);
+	}
 
-    if (city) {
-        query += ' AND f.CITY LIKE ?';
-        params.push(`%${city}%`);
-    }
+	if (city) {
+		query += ' AND f.CITY LIKE ?';
+		params.push(`%${city}%`);
+	}
 
-    if (category) {
-        query += ' AND c.NAME LIKE ?';
-        params.push(`%${category}%`);
-    }
+	if (category) {
+		query += ' AND c.NAME LIKE ?';
+		params.push(`%${category}%`);
+	}
 
-    connection.query(query, params, (err, records, fields) => {
-        if (err) {
-            console.error("Error while searching fundraisers:", err);
-            res.status(500).json({ error: 'An error occurred while searching fundraisers' });
-        } else {
-            res.json(records);
-        }
-    });
-});
-
+	connection.query(query, params, (err, records, fields) => {
+		if (err) {
+			console.error("Error while retrieve the data");
+		} else {
+			res.send(records);
+		}
+	})
+})
 
 
 
-// router.get("/:id", (req, res)=>{
-// 	connection.query("select * from concert where ConcertID=" + req.params.id, (err, records,fields)=> {
-// 		 if (err){
-// 			 console.error("Error while retrieve the data");
-// 		 }else{
-// 			 res.send(records);
-// 		 }
-// 	})
-// })
+
+
+router.get("/fundraiser/:id", (req, res)=>{
+	connection.query("select * from FUNDRAISER where FUNDRAISER_ID=" + req.params.id, (err, records,fields)=> {
+		 if (err){
+			 console.error("Error while retrieve the data");
+		 }else{
+			 res.send(records);
+		 }
+	})
+})
 
 
 
